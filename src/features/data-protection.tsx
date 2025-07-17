c06eaa3971dee14af6dd5e7f994f1eeb9aeb9dc1
FS <f.scholze@gmx.de>
2025-07-17 14:16:25 +0200

    Update background color

--- src/features/data-protection.tsx
+++ src/features/data-protection.tsx
@@ -8,7 +8,7 @@ const DataProtection: FC<{}> = () => {
       maxWidth='lg'
       sx={{
         padding: 5,
-        backgroundColor: 'rgba(255, 255, 255, 0.6)',
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
         borderRadius: 5,
         maxHeight: '100%',
         justifyContent: 'center',
--- src/features/imprint.tsx
+++ src/features/imprint.tsx
@@ -9,7 +9,7 @@ const Imprint: FC<{}> = () => {
       maxWidth='lg'
       sx={{
         padding: 5,
-        backgroundColor: 'rgba(255, 255, 255, 0.6)',
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
         borderRadius: 5,
         maxHeight: '100%',
         justifyContent: 'center',
